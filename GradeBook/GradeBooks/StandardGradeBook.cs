23bf941f2ea73b1d1b3c45dd3f5be0dfed8a84be
Ahmad EL Masri <ahmadjelmasri@gmail.com>
2018-03-09 05:38:09 -0500

    fix: typo

--- GradeBook/GradeBooks/StandardGradeBook.cs
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -4,7 +4,7 @@ namespace GradeBook.GradeBooks
 {
     public class StandardGradeBook : BaseGradeBook 
     {
-         publc StandardGradeBook(string name) : base(name)
+        public StandardGradeBook(string name) : base(name)
          {
             Type = GradeBookType.Standard;
          }
